c506e4d2637b7a2206a396c0b6153279991de4f2
Nisha Yadav <93nishayadav@gmail.com>
2018-08-02 11:02:00 +0530

    Added comments

--- navigation/index.js
+++ navigation/index.js
@@ -1,22 +1,24 @@
 $(document).ready(()=>{
-$(window).trigger("resize");
+    //check for windows size on document.ready and act accordingly
+    $(window).trigger("resize");
 });
 
+//change he orientation as well as show /hide image and nav menu on image resize
 $(window).resize(function(){
-
-if($(window).width()<768){
-$('ul').hide();
-$('img').show();
-$('ul').addClass("vertical-nav");
-}
-else
-{
-	$('ul').removeClass("vertical-nav");
-$('img').hide();
-$('ul').show();
-}
+    if($(window).width()<768){
+        $('ul').hide();
+        $('img').show();
+        $('ul').addClass("vertical-nav");
+    }
+    else
+    {
+	    $('ul').removeClass("vertical-nav");
+        $('img').hide();
+        $('ul').show();
+    }
 });
 
+//show/hide nav menu on image click
 $('img').click(function(){
-$('ul').toggle();
+    $('ul').toggle();
 });
