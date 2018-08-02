e01957c9f7f0415cc46d5ea2487173d6bfa5517a
Nisha Yadav <93nishayadav@gmail.com>
2018-08-02 11:04:36 +0530

    added comments

--- popup/index.js
+++ popup/index.js
@@ -1,25 +1,29 @@
+//Check size of screen in document ready to manage the size of modal
 $(document).ready(()=>{
 	$(window).trigger("resize");
 });
+
+//Button click to trigger modal open
 $('#button1').click(function(){
-$('#button1').hide();
-$('body').toggleClass("over");
-$('#myModal').toggleClass("modal");
+   $('#button1').hide();
+   $('body').toggleClass("over");
+   $('#myModal').toggleClass("modal");
 });
 
+//Close button of modal to close modal and overlay
 $('#close').click(function(){
-$('#button1').show();
-$('body').toggleClass("over");
-$('#myModal').toggleClass("modal");
-
+   $('#button1').show();
+   $('body').toggleClass("over");
+   $('#myModal').toggleClass("modal");
 });
+//Modal size should be 100% for mobile screens
 $(window).resize(function(){
-if($(window).width()<768){
-$('.content').css("width","100%");
-}
-else
-{
+   if($(window).width()<768){
+   $('.content').css("width","90%");
+   }
+   else
+   {
 	$('.content').css("width","50%");
-}
+   }
 });
 
